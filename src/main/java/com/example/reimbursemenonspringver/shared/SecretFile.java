d9464d92935646695c5dcc6d33f9f14871248fe7
Devon <71043147+devoncowherd@users.noreply.github.com>
2022-09-12 00:05:01 -0700

    Delete SecretFile.java

--- src/main/java/com/example/reimbursemenonspringver/shared/SecretFile.java
+++
@@ -1,7 +0,0 @@
-package com.example.reimbursemenonspringver.shared;
-
-public class SecretFile {
-    static String dbPassword = "ErinLessThan3!";
-    static String dbUsername = "root";
-    static String dbURL = "jdbc:mysql://localhost:3306/baat";
-}
